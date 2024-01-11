bd5ee08f31f479a8cce7f7c2213ec5f2dc2f2020
Matheus Paice <matheus.paice@jbtec.com.br>
2024-01-11 19:26:45 -0300

    feat: stable

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
