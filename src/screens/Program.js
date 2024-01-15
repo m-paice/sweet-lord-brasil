6606bae403e4c749336216c64394aa3f684e6f59
Matheus Paice <matheus.paice@jbtec.com.br>
2024-01-15 13:58:23 -0300

    feat: add completed program

--- src/screens/Program.js
+++ src/screens/Program.js
@@ -9,45 +9,64 @@ import { Header } from "../components/Header";
 const programations = [
   {
     id: 1,
-    day: "Domingo",
-    time: "06:00 às 08:00",
-    name: "Programa 1",
+    day: "Segunda-feira",
+    schedules: [
+      { name: "Special Classics", time: "08:30 - 20:00" },
+      { name: "WORLD JAZZ", time: "20:00 - 00:00" },
+    ],
   },
   {
     id: 2,
-    day: "Segunda-feira",
-    time: "06:00 às 08:00",
-    name: "Programa 2",
+    day: "Terça-feira",
+    schedules: [
+      { name: "Special Classics", time: "00:00 - 21:00" },
+      { name: "SWEET BALLADS	", time: "21:00 - 00:15" },
+    ],
   },
   {
     id: 3,
-    day: "Terça-feira",
-    time: "06:00 às 08:00",
-    name: "Programa 3",
+    day: "Quarta-feira",
+    schedules: [
+      { name: "Special Classics", time: "00:15 - 21:00" },
+      { name: "UNPLUGGED CLASSICS", time: "21:00 - 00:00" },
+    ],
   },
   {
     id: 4,
-    day: "Quarta-feira",
-    time: "06:00 às 08:00",
-    name: "Programa 4",
+    day: "Quinta-feira",
+    schedules: [
+      { name: "Special Classics", time: "00:00 - 21:00" },
+      { name: "SWEET BALLADS", time: "21:00 - 00:30" },
+    ],
   },
   {
     id: 5,
-    day: "Quinta-feira",
-    time: "06:00 às 08:00",
-    name: "Programa 5",
+    day: "Sexta-feira",
+    schedules: [
+      { name: "Special Classics", time: "00:30 - 21:00" },
+      { name: "BLUES, SOUL AND R&B", time: "21:00 - 00:00" },
+    ],
   },
   {
     id: 6,
-    day: "Sexta-feira",
-    time: "06:00 às 08:00",
-    name: "Programa 6",
+    day: "Sábado",
+    schedules: [
+      { name: "Special Classics", time: "00:10 - 07:00" },
+      { name: "COUNTRY CLASSICS", time: "07:00 - 14:00" },
+      { name: "ROCK BRASIL", time: "14:00 - 18:00" },
+      { name: "RAÍZES - MPB", time: "18:00 - 21:00" },
+      { name: "NIGHT SOUL", time: "21:00 - 00:30" },
+    ],
   },
   {
     id: 7,
-    day: "Sábado",
-    time: "06:00 às 08:00",
-    name: "Programa 7",
+    day: "Domingo",
+    schedules: [
+      { name: "Special Classics", time: "00:30 - 10:00" },
+      { name: "SWEET BALLADS", time: "10:00 - 15:50" },
+      { name: "SPECIAL CLASSICS", time: "19:00 - 22:00" },
+      { name: "SOFT ROCK", time: "22:00 - 08:30" },
+    ],
   },
 ];
 
@@ -63,32 +82,27 @@ export const Program = () => {
         <ThemedText size="sm" color="secondary">
           Fique ligado por tudo que esta tocando na rádio
         </ThemedText>
+      </View>
 
-        <ScrollView style={{ marginTop: 30 }}>
-          {programations.map((programation) => (
-            <View
-              key={programation.id}
-              style={{
-                borderBottomWidth: 1,
-                borderColor: "#FBF2C0",
-                marginBottom: 10,
-              }}
-            >
-              <ThemedText size="sm" color="tertiary">
-                {programation.day}
-              </ThemedText>
-              <Box between row>
+      <ScrollView style={{ marginVertical: 20 }}>
+        {programations.map((programation) => (
+          <View key={programation.id} style={styles.item}>
+            <ThemedText size="sm" color="tertiary">
+              {programation.day}
+            </ThemedText>
+            {programation.schedules.map((item) => (
+              <Box key={item.name} between row>
                 <ThemedText size="sm" color="secondary">
-                  {programation.name}
+                  {item.name}
                 </ThemedText>
                 <ThemedText size="xsm" color="tertiary">
-                  {programation.time}
+                  {item.time}
                 </ThemedText>
               </Box>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
     </LinearGradient>
   );
 };
@@ -99,7 +113,10 @@ const styles = StyleSheet.create({
     paddingVertical: 40,
     paddingHorizontal: 20,
   },
-  spacing: {
-    marginVertical: 20,
+
+  item: {
+    borderBottomWidth: 1,
+    borderColor: "#FBF2C0",
+    marginBottom: 10,
   },
 });
